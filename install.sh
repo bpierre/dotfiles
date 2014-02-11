e9cc125a992669d1c95369d86713835cb1e8b92a
Pierre Bertet <bonjour@pierrebertet.net>
2014-02-11 19:02:28 +0000

    Files renaming

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s ${DIR}/vimrc.local ~/.vimrc.local
-ln -s ${DIR}/gvimrc.local ~/.gvimrc.local
+ln -s ${DIR}/vimrc ~/.vimrc.local
+ln -s ${DIR}/gvimrc ~/.gvimrc.local
 ln -s ${DIR}/.css ~/.css
 ln -s ${DIR}/.js ~/.js
