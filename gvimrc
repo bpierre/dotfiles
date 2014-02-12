7dbc802adb51f0fa5dd550640ecbd3f0eae3a750
Pierre Bertet <bonjour@pierrebertet.net>
2014-02-12 22:57:19 +0000

    Removed gvimrc

--- gvimrc
+++
@@ -1,8 +0,0 @@
-set guioptions=aAce
-syntax enable
-
-set background=dark
-colorscheme desert
-"set guifont=Anonymous\ Pro:h14
-set guifont=SourceCodePro-Medium:h14
-
--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s ${DIR}/vimrc ~/.vimrc.local
-ln -s ${DIR}/gvimrc ~/.gvimrc.local
 ln -s ${DIR}/.css ~/.css
 ln -s ${DIR}/.js ~/.js
