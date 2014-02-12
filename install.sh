784a419fed52141a5f0b8bbaac7b0fc1f8986891
Pierre Bertet <bonjour@pierrebertet.net>
2014-02-12 23:01:36 +0000

    Updated the install script

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -s ${DIR}/vimrc ~/.vimrc.local
+ln -s ${DIR}/tmux ~/.tmux.conf
+ln -s ${DIR}/zshrc ~/.zshrc
+ln -s ${DIR}/zpreztorc ~/.zpreztorc
 ln -s ${DIR}/.css ~/.css
 ln -s ${DIR}/.js ~/.js
