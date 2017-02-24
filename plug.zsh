8e2d67cb622898a4a106453a1d91fced13c26e1d
Pierre Bertet <bonjour@pierrebertet.net>
2017-02-24 22:31:34 +0000

    Plug loading as a separate script

---
+++ plug.zsh
@@ -0,0 +1,13 @@
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-syntax-highlighting", defer:2 # Should be loaded 2nd last.
+zplug "zsh-users/zsh-history-substring-search", defer:3 # Should be loaded last.
+
+zplug "mafredri/zsh-async", from:github
+zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+zplug "modules/osx", from:prezto
+
+zplug load
