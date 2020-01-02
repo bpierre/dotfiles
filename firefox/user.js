acc8e8a1571e98c3bcbd35e39af8a4c7f1873ddd
Pierre Bertet <hello@pierre.world>
2020-01-02 18:00:46 +0100

    Add Firefox user.js

---
+++ firefox/user.js
@@ -0,0 +1,38 @@
+// Important: keep the semicolons or a parsing
+// error will prevent the file to be executed.
+user_pref('widget.content.gtk-theme-override', 'Adwaita');
+user_pref('browser.fixup.alternate.enabled', false);
+user_pref('browser.urlbar.trimURLs', false);
+user_pref('general.warnOnAboutConfig', false);
+user_pref('layout.css.devPixelsPerPx', '2.5');
+user_pref('ui.key.menuAccessKeyFocuses', false);
+user_pref('devtools.theme', 'dark');
+user_pref('browser.urlbar.clickSelectsAll', true);
+user_pref('browser.urlbar.doubleClickSelectsAll', false);
+user_pref('layout.word_select.stop_at_punctuation', true);
+user_pref('media.mediasource.ignore_codecs', true);
+user_pref('full-screen-api.warning.timeout', 0);
+user_pref('image.mem.min_discard_timeout_ms', 2100000000);
+user_pref('image.mem.max_decoded_image_kb', 30000);
+
+user_pref('gfx.font_rendering.fontconfig.max_generic_substitutions', 127);
+
+// Acceleration
+user_pref('gfx.webrender.all', true);
+user_pref('gfx.webrender.enabled', true);
+// This can cause issues with the Nvidia driver
+user_pref('layers.acceleration.force-enabled', true);
+
+// Scroll
+user_pref('mousewheel.min_line_scroll_amount', 40);
+user_pref('general.smoothScroll', false);
+user_pref('general.smoothScroll.pages', false);
+// user_pref("general.smoothScroll.lines.durationMaxMS", 125);
+// user_pref("general.smoothScroll.lines.durationMinMS", 125);
+// user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
+// user_pref("general.smoothScroll.mouseWheel.durationMinMS", 100);
+// user_pref("general.smoothScroll.msdPhysics.enabled", true);
+// user_pref("general.smoothScroll.other.durationMaxMS", 125);
+// user_pref("general.smoothScroll.other.durationMinMS", 125);
+// user_pref("general.smoothScroll.pages.durationMaxMS", 125);
+// user_pref("general.smoothScroll.pages.durationMinMS", 125);
