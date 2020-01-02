a7390b88f5af055ec3f0649d88b456b9403bf40a
Pierre Bertet <hello@pierre.world>
2020-01-02 18:02:52 +0100

    Update install.sh to reflect current status

--- install.sh
+++ install.sh
@@ -14,36 +14,32 @@ linkFile() {
   /bin/echo -e "$msg"
 }
 
-mkdir -p $SCRIPTDIR/vendor
-if [ ! -d $SCRIPTDIR/vendor/z ] ; then
-  /bin/echo -e "[\e[0;32mz\e[0m] cloning into $SCRIPTDIR/vendor/z..."
-  git clone git@github.com:rupa/z.git $SCRIPTDIR/vendor/z
-else
-  /bin/echo -e "[\e[0;36mz\e[0m] already cloned"
-fi
-if [ ! -d $SCRIPTDIR/vendor/fzf ] ; then
-  /bin/echo -e "[\e[0;32mz\e[0m] cloning into $SCRIPTDIR/vendor/fzf..."
-  git clone git@github.com:junegunn/fzf.git $SCRIPTDIR/vendor/fzf
-else
-  /bin/echo -e "[\e[0;36mz\e[0m] already cloned"
-fi
+#mkdir -p $SCRIPTDIR/vendor
+#if [ ! -d $SCRIPTDIR/vendor/z ] ; then
+#  /bin/echo -e "[\e[0;32mz\e[0m] cloning into $SCRIPTDIR/vendor/z..."
+#  git clone git@github.com:rupa/z.git $SCRIPTDIR/vendor/z
+#else
+#  /bin/echo -e "[\e[0;36mz\e[0m] already cloned"
+#fi
+#if [ ! -d $SCRIPTDIR/vendor/fzf ] ; then
+#  /bin/echo -e "[\e[0;32mz\e[0m] cloning into $SCRIPTDIR/vendor/fzf..."
+#  git clone git@github.com:junegunn/fzf.git $SCRIPTDIR/vendor/fzf
+#else
+#  /bin/echo -e "[\e[0;36mz\e[0m] already cloned"
+#fi
 
 # TODO: use GNU stow
 
 linkFile vimrc     ~/.vimrc
 linkFile vimrc     ~/.nvimrc
+
 linkFile vim       ~/.vim
 linkFile vim       ~/.nvim
+linkFile vim       ~/.config/nvim
+
 linkFile tmux.conf ~/.tmux.conf
-#linkFile tmux-osx.conf ~/.tmux-osx.conf
 linkFile zshrc     ~/.zshrc
 linkFile zprofile  ~/.zprofile
 linkFile zshenv    ~/.zshenv
-linkFile zpreztorc ~/.zpreztorc
 linkFile zlogin    ~/.zlogin
-#linkFile ackrc     ~/.ackrc
 linkFile gitignore ~/.gitignore
-linkFile dotjs/css ~/.css
-linkFile dotjs/js  ~/.js
-
-./vendor/fzf/install
