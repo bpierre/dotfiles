087ac882f15cde399ce46b9239688222131dbee6
Pierre Bertet <hello@pierre.world>
2021-12-15 16:22:11 +0000

    Firefox config update

--- firefox/user.js
+++ firefox/user.js
@@ -45,6 +45,23 @@ user_pref('gfx.webrender.all', true);
 user_pref('gfx.webrender.enabled', true);
 user_pref('layers.acceleration.force-enabled', true);
 
+// VP9 video acceleration
+user_pref('media.ffmpeg.vaapi.enabled', true);
+user_pref('media.ffmpeg.vaapi-drm-display.enabled', true);
+
+// Enable hardware VA-API decoding for WebRTC
+user_pref('media.navigator.mediadatadecoder_vpx_enabled', true);
+
+// Use libvpx instead of ffvpx to decode vp9
+user_pref('media.ffvpx.enabled', false);
+
+// Disable the remote data decoder process for VP8/VP9
+user_pref('media.rdd-vpx.enabled', false);
+
+// Disable EGL on X11 (see also MOZ_X11_EGL=1 in ~/.profile)
+user_pref('gfx.x11-egl.force-disabled', false);
+user_pref('gfx.x11-egl.force-enabled', true);
+
 // Scroll
 user_pref('mousewheel.min_line_scroll_amount', 40);
 user_pref('general.smoothScroll', false);
